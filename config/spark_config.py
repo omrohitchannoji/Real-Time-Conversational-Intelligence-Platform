b22407f2915e55dad7273dffed310be42f93867e
omrohitchannoji <omrohitchannoji7@gmail.com>
2026-09-13 03:24:24 +0530

    Dynamically match spark-sql-kafka version to installed PySpark

--- config/spark_config.py
+++ config/spark_config.py
@@ -1,10 +1,13 @@
 import os
+import pyspark
 from dotenv import load_dotenv
 
 load_dotenv()
 
 APP_NAME = "RealTimeConversationalContextPipeline"
 CHECKPOINT_LOCATION = os.getenv("SPARK_CHECKPOINT_DIR", "./checkpoints")
-KAFKA_PACKAGE = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5"
+spark_ver = getattr(pyspark, "__version__", "3.5.0")
+KAFKA_PACKAGE = f"org.apache.spark:spark-sql-kafka-0-10_2.12:{spark_ver}"
 
 SPARK_LOG_LEVEL = "ERROR"
+
