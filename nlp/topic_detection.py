b157aa8268ee889e40f4a8390ad9715cc583b7da
omrohitchannoji <omrohitchannoji7@gmail.com>
2026-09-23 02:04:36 +0530

    fix: add placeholder words to STOPWORDS in topic_detection.py

--- nlp/topic_detection.py
+++ nlp/topic_detection.py
@@ -30,7 +30,8 @@ STOPWORDS = {
     "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", 
     "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "whatever", "when", "when's", 
     "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", 
-    "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
+    "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves",
+    "deleted", "removed", "missing", "placeholder", "content", "unavailable"
 }
 
 
