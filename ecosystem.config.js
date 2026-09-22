8cf54169d76a643c5a3f46819cc5a6c4861dc8dd
omrohitchannoji <omrohitchannoji7@gmail.com>
2026-09-23 00:27:40 +0530

    Add absolute cwd and PYTHONPATH to PM2 ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,18 +3,20 @@ module.exports = {
     {
       name: 'kafka-producer',
       script: 'kafka_pipeline/producer.py',
+      cwd: '/home/ubuntu/Real-Time-Conversational-Intelligence-Platform',
       interpreter: 'python3',
       autorestart: false,
       env: {
-        PYTHONPATH: '.'
+        PYTHONPATH: '/home/ubuntu/Real-Time-Conversational-Intelligence-Platform:.'
       }
     },
     {
       name: 'pyspark-consumer',
       script: 'spark/spark_consumer.py',
+      cwd: '/home/ubuntu/Real-Time-Conversational-Intelligence-Platform',
       interpreter: 'python3',
       env: {
-        PYTHONPATH: '.'
+        PYTHONPATH: '/home/ubuntu/Real-Time-Conversational-Intelligence-Platform:.'
       }
     }
   ]
